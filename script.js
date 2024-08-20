6b859b15dbd0d0c62a19aa042e01b0ec52fab48c
Bia castro <anabiafernandes.222@gmail.com>
2024-08-20 20:34:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = prompt( "Qual é o seu primiro nome?")
+let sobrenome = prompt( "Qual é o seu sobrenome?")
+let curso = prompt( "Qual é o seu curso?")
+const anoNacimento = prompt( "Qual é o seu ano de nacimento?")
+
+const idade = 2024-anoNacimento
+
+alert(`olá ${nome} ${sobrenome} Então você estuda ${curso} e você tem ${idade} anos. Legal!`)
