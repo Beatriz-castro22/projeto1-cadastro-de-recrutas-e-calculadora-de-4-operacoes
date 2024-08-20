eb7e7dffe612e2582ce71ee3787bbf8d57c3c4f2
Bia castro <anabiafernandes.222@gmail.com>
2024-08-20 20:46:22 -0300

    Add files via upload

---
+++ script (2).js
@@ -0,0 +1,12 @@
+let num1 = parseInt(prompt("Digite o primeiro numero"))
+let num2 = parseInt(prompt("Digite o segundo numero"))
+let soma = num1 + num2
+let sub = num1 - num2
+let mult = num1 * num2
+let div = num1 / num2
+
+window.alert(`resultado:
+${num1} + ${num2} = ${soma}
+${num1} - ${num2} = ${sub}
+${num1} * ${num2} = ${mult}
+${num1} / ${num2} = ${div}`)
